61455c3a61316a49df1b2b0fbe241ba2ed2269f9
PRANITHMEESALA <47515348+PRANITHMEESALA@users.noreply.github.com>
2019-08-03 11:23:02 +0530

    ch auxi

--- PersonNameHandler/src/components/Cockpit/Cockpit.js
+++ PersonNameHandler/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Styles from '../Cockpit.module.css';
-import Aux from '../../hoc/Aux';
+import Auxi from '../../hoc/Auxi';
 const cockpit=(props)=>{
     const classes = [];
     let btnClass=Styles.Button;
@@ -15,14 +15,14 @@ const cockpit=(props)=>{
       classes.push(Styles.bold);
     }
   return(
-      <Aux>
+      <Auxi>
         <h1>{props.appTitle}</h1>
     <p className={classes.join( ' ' )}> This is working </p>
      <button
       className={btnClass}
       onClick={props.click}>Toggle Persons</button>
       <button onClick={props.login}> log in </button>      
-      </Aux>
+      </Auxi>
   );
 };
 export default cockpit;
